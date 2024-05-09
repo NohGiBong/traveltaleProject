3d48cfb7697d444e3da8277997171db33618b3d6
NohGiBong <nodarbong@gmail.com>
2024-05-09 13:40:21 +0900

    테일 완료

--- app/src/main/java/com/example/traveltaleproject/tale/TaleWriteActivity.kt
+++ app/src/main/java/com/example/traveltaleproject/tale/TaleWriteActivity.kt
@@ -28,114 +28,121 @@ class TaleWriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTaleWriteBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        // 데이터 초기화 펑션 호출
-        initializeData()
-
-        // 상위 게시물에서 기존 정보 받아오는 평선 호출
-        fetchTravelListData()
-
-        // 수정을 위해 기존 텍스트 받아오는 펑션 호출
-        fetchTaleData()
-
-        // 레이아웃 저장버튼 바인딩 & 저장 펑션 호출
-        binding.saveTaleButton.setOnClickListener {
-            val taleText = binding.taleWrite.text.toString()
-            if (taleText.isNotEmpty()) {
-                saveTaleToDatabase(taleText)
-            }
-        }
+        // 주요 기능 호출
+        setupUI()
+        initializeFirebase()
+        loadData()
     }
 
-    //데이터 초기화 펑션
-    private fun initializeData() {
+    // UI 데이터 받아오기
+    private fun setupUI() {
+        binding = ActivityTaleWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         sharedPreferences = getSharedPreferences("MyInfo", MODE_PRIVATE)
         userId = getSessionId()
         travelListId = intent.getStringExtra("travelListId") ?: ""
         talesid = intent.getStringExtra("talesid")
-        databaseReference = FirebaseDatabase.getInstance().reference.child("TravelList").child(userId).child(travelListId)
-    }
 
-    //상위 게시물에서 기존 정보 받아오는 평선
-    private fun fetchTravelListData() {
-        databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                //상위 게시물에서 기존 정보 받아오는 상세 펑션 호출
-                setTravelListData(snapshot)
+        binding.saveTaleButton.setOnClickListener {
+            val taleText = binding.taleWrite.text.toString()
+            if (taleText.isNotEmpty()) {
+                val operation = if (talesid != null) ::updateTaleInDatabase else ::saveTaleToDatabase
+                operation(taleText)
             }
-            override fun onCancelled(error: DatabaseError) {
-            }
-        })
-    }
-
-    // 수정을 위해 기존 텍스트 받아오는 펑션
-    private fun fetchTaleData() {
-        if (talesid != null) {
-            databaseReference.child("tales").child(talesid!!).addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val taleData = snapshot.getValue(TaleData::class.java)
-                    taleData?.let { binding.taleWrite.setText(it.text) }
-                }
-                override fun onCancelled(error: DatabaseError) {
-                }
-            })
         }
     }
 
-    // DB 저장 펑션
+    // DB 초기화
+    private fun initializeFirebase() {
+        databaseReference = FirebaseDatabase.getInstance().reference
+            .child("TravelList").child(userId).child(travelListId)
+    }
+
+    // 상위 게시물 불러오기 호출 후 현재 테일에 데이터가 있다면 현재 게시물 불러오기도 호출
+    private fun loadData() {
+        fetchTravelListData()
+        talesid?.let { fetchTaleData(it) }
+    }
+
+    // DB에 저장
     private fun saveTaleToDatabase(taleText: String) {
-        val newtalesid = generateUniqueId()
-        val taleData = TaleData(talesid = newtalesid, text = taleText)
-        databaseReference.child("tales").child(newtalesid).setValue(taleData)
-            // 저장 성공 시 테일 상세 액티비티로 데이터를 넘기고 이동하는 펑션 호출
+        val newTalesId = generateUniqueId()
+        val taleData = TaleData(talesid = newTalesId, text = taleText)
+        persistTaleData(newTalesId, taleData)
+    }
+
+    // DB에 수정 저장
+    private fun updateTaleInDatabase(taleText: String) {
+        talesid?.let {
+            val taleData = TaleData(talesid = it, text = taleText)
+            persistTaleData(it, taleData)
+        }
+    }
+
+    // 데이터 영속성 관리
+    private fun persistTaleData(talesId: String, taleData: TaleData) {
+        databaseReference.child("tales").child(talesId).setValue(taleData)
             .addOnSuccessListener { navigateToTaleGetActivity(taleData) }
-            .addOnFailureListener { showToast("테일 저장 실패: ${it.message}") }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "테일 저장 실패: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
-    //상위 게시물에서 기존 정보 받아오는 상세 펑션
-    private fun setTravelListData(snapshot: DataSnapshot) {
-        val title = snapshot.child("title").value.toString()
-        val startDate = snapshot.child("startDate").value as Long
-        val endDate = snapshot.child("endDate").value as Long
-        val address = snapshot.child("address").value.toString()
-        val travelImage = snapshot.child("travelImage").value.toString()
-
-        binding.taleWriteTitle.setText(title)
-        binding.startDateTxt.text = formatDate(startDate)
-        binding.endDateTxt.text = formatDate(endDate)
-        binding.mapTxt.text = address
-        Picasso.get().load(travelImage).into(binding.mainImg)
-    }
-
-    // 날짜 형식 지정 펑션
-    private fun formatDate(timestamp: Long): String {
-        val sdf = SimpleDateFormat("dd.MMM.yyyy", Locale.ENGLISH)
-        return sdf.format(Date(timestamp))
-    }
-
-    // 사용자 정보 받아오는 펑션
-    private fun getSessionId(): String {
-        return sharedPreferences.getString("user_id", "").toString()
-    }
-
-    // 새로 작성하는 테일에 랜덤 ID를 부여하는 펑션
-    private fun generateUniqueId(): String {
-        return UUID.randomUUID().toString()
-    }
-
-    // 테일 상세 액티비티로 데이터를 넘기고 이동하는 펑션
+    // 저장 성공 시 데이터 전달 및 상세 페이지 이동
     private fun navigateToTaleGetActivity(taleData: TaleData) {
-        val intent = Intent(this, TaleGetActivity::class.java)
-        intent.putExtra("taleData", taleData)
-        intent.putExtra("travelListId", travelListId)
-        startActivity(intent)
-        finish()
+        Intent(this, TaleGetActivity::class.java).also {
+            it.putExtra("taleData", taleData)
+            it.putExtra("travelListId", travelListId)
+            startActivity(it)
+            finish()
+        }
     }
 
-    // 토스트 펑션
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    // 현재 게시물 데이터 불러오기
+    private fun fetchTaleData(talesId: String) {
+        databaseReference.child("tales").child(talesId)
+            .addListenerForSingleValueEvent(valueEventListener { snapshot ->
+                val taleData = snapshot.getValue(TaleData::class.java)
+                taleData?.let { binding.taleWrite.setText(it.text) }
+            })
+    }
+
+    // 상위 게시물 데이터 불러오기 및 UI 업데이트
+    private fun fetchTravelListData() {
+        databaseReference.addListenerForSingleValueEvent(valueEventListener { snapshot ->
+            val title = snapshot.child("title").value.toString()
+            val startDate = snapshot.child("startDate").value as Long
+            val endDate = snapshot.child("endDate").value as Long
+            val address = snapshot.child("address").value.toString()
+            val travelImage = snapshot.child("travelImage").value.toString()
+
+            binding.taleWriteTitle.setText(title)
+
+            val sdf = SimpleDateFormat("dd.MMM.yyyy", Locale.ENGLISH)
+            val formattedStartDate = sdf.format(Date(startDate))
+            val formattedEndDate = sdf.format(Date(endDate))
+
+            binding.startDateTxt.text = formattedStartDate
+            binding.endDateTxt.text = formattedEndDate
+            binding.mapTxt.text = address
+
+            Picasso.get().load(travelImage)
+
+        })
+    }
+
+    // 사용자 정보 획득
+    private fun getSessionId() = sharedPreferences.getString("user_id", "")!!
+
+    // 테일 고유 ID 생성
+    private fun generateUniqueId() = UUID.randomUUID().toString()
+
+    // 데이터 불러오기 리스너
+    private fun valueEventListener(onDataChangeHandler: (DataSnapshot) -> Unit): ValueEventListener {
+        return object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) = onDataChangeHandler(snapshot)
+            override fun onCancelled(error: DatabaseError) {}
+        }
     }
 }
